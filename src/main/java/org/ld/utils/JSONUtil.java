5e05095e23f4c1a7a18eb38166bece77281bf024
liuxiaohei <2297598383@qq.com>
2019-09-19 23:46:53 +0800

    adj

--- src/main/java/org/ld/utils/JSONUtil.java
+++ src/main/java/org/ld/utils/JSONUtil.java
@@ -1,30 +1,18 @@
 package org.ld.utils;
 
-
-import com.google.gson.*;
 import org.springframework.lang.NonNull;
 
 
 public class JSONUtil {
     private static final Logger logger = Logger.newInstance(JSONUtil.class);
 
-    private static final Gson GSON = new GsonBuilder().create();
-    private static final Gson GSON_PRETTY = new GsonBuilder().setPrettyPrinting().create();
-
 
     /**
      * 将json字符串转换为对象
      */
     public static <T> T json2Obj(String json, @NonNull Class<T> c) {
-        if (isEmptyJson(json)) {
-            return null;
-        }
-        try {
-            return GSON.fromJson(json, c);
-        } catch (Exception e) {
-            logger.printStackTrace(e);
-            return null;
-        }
+        //todo
+        return null;
     }
 
     /**
--- src/test/java/org/ld/examples/DemoTest.java
+++ src/test/java/org/ld/examples/DemoTest.java
@@ -4,19 +4,6 @@ import org.junit.Test;
 import org.ld.enums.ErrorCodeEnum;
 import org.ld.exception.ErrorCode;
 
-import java.net.PasswordAuthentication;
-import java.util.Properties;
-
-import java.util.Date;
-
-
-import javax.mail.Authenticator;
-import javax.mail.Message;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-
 public class DemoTest {
 
     @Test
