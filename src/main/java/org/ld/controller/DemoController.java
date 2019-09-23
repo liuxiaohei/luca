7141affdbf0035279469534a687af0bda90c40e4
liuxiaohei <2297598383@qq.com>
2019-09-23 14:35:39 +0800

    adj

--- src/main/java/org/ld/controller/DemoController.java
+++ src/main/java/org/ld/controller/DemoController.java
@@ -3,13 +3,11 @@ package org.ld.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 
 /**
  */
@@ -19,7 +17,6 @@ public class DemoController {
 
     @ApiOperation(value = "事例", produces = "application/json")
     @GetMapping(value = "demo")
-    @ResponseBody
     public Object demo() {
         Map<String, Object> a = new HashMap<>();
         Map<String, Object> b = new HashMap<>();
