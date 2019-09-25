b3455558ec7bcdee4f2664e2fefdde1f9436f9b5
liuxiaohei <2297598383@qq.com>
2019-09-25 15:17:25 +0800

    adj

--- src/test/java/org/ld/examples/DemoTest.java
+++ src/test/java/org/ld/examples/DemoTest.java
@@ -3,6 +3,10 @@ package org.ld.examples;
 import org.junit.Test;
 import org.ld.enums.SystemErrorCodeEnum;
 import org.ld.exception.ErrorCode;
+import org.ld.utils.Logger;
+import org.ld.utils.UuidUtils;
+
+import java.util.stream.Stream;
 
 public class DemoTest {
 
@@ -14,5 +18,14 @@ public class DemoTest {
     @Test
     public void sendMail() {
 
+    }
+
+    /**
+     * 无限流
+     */
+    @Test
+    public void infiniteStream() {
+        //Stream.iterate(0, i -> ++i).limit(1000).forEach(e -> Logger.newInstance().info(() -> "" + e));
+        Stream.generate(UuidUtils::getShortUuid).limit(1000).forEach(e -> Logger.newInstance().info(() -> "" + e));
     }
 }
