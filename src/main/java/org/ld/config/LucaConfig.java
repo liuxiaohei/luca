1d6267f28dce59be5412645da01311aeb82e753c
liuxiaohei <2297598383@qq.com>
2019-09-25 10:23:20 +0800

    adj

--- src/main/java/org/ld/config/LucaConfig.java
+++ src/main/java/org/ld/config/LucaConfig.java
@@ -3,14 +3,23 @@ package org.ld.config;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 import javax.servlet.MultipartConfigElement;
 
 /**
  * 获取配置中心的自定义配置信息
+ *
  * @author ld
  */
 @SuppressWarnings("unused")
 @Configuration
+@EnableSwagger2
 public class LucaConfig {
 
     /**
@@ -22,5 +31,21 @@ public class LucaConfig {
         factory.setMaxFileSize("20480KB");
         factory.setMaxRequestSize("102400KB");
         return factory.createMultipartConfig();
+    }
+
+    /**
+     * http://127.0.0.1:9999/swagger-ui.html#/
+     */
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(new ApiInfoBuilder()
+                        .title("LUCA API")
+                        .version("1.0")
+                        .build());
     }
 }
