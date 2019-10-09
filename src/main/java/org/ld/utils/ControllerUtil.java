9705802bc9d51f6b126565a9aee1e8ac0af6503e
liuxiaohei <2297598383@qq.com>
2019-10-09 23:30:08 +0800

    adj

--- src/main/java/org/ld/utils/ControllerUtil.java
+++ src/main/java/org/ld/utils/ControllerUtil.java
@@ -5,15 +5,14 @@ import org.ld.enums.SystemErrorCodeEnum;
 import org.ld.exception.CodeStackException;
 import org.ld.exception.ErrorCode;
 import org.ld.functions.UncheckedSupplier;
-import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import scala.Enumeration;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 public class ControllerUtil {
 
@@ -23,55 +22,51 @@ public class ControllerUtil {
      * 转换响应结构体
      */
     public static Object convertResponseBody(UncheckedSupplier point) {
-        ResponseEntity<Object> responseEntity;
-        ResponseBodyBean<Object> result = new ResponseBodyBean<>();
         try {
-            final Object body = point.get();
-            result.setData(body);
+            ResponseBodyBean<Object> result = new ResponseBodyBean<>();
+            result.setData(point.get());
             result.setErrorCode(SystemErrorCodeEnum.SUCCESS().id());
             result.setMessage(SystemErrorCodeEnum.SUCCESS().toString());
             LOG.info(() -> "Response Body : " + JsonUtil.obj2Json(result));
-            responseEntity = new ResponseEntity<>(result,HttpStatus.OK);
+            return new ResponseEntity<>(result, HttpStatus.OK);
         } catch (Throwable e) {
-            if (AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class) != null) {
-                throw new CodeStackException(e);
-            }
             LOG.printStackTrace(e);
-            if (e instanceof OutOfMemoryError) {
-                LOG.error(() -> "OutOfMemoryError:", e);
-                System.exit(0);
-                return null;
-            }
             final CodeStackException se = Optional.ofNullable(findException(e))
                     .orElseGet(() -> ExceptionUtil.getSystemErrorValue(e)
                             .map(ErrorCode::new)
                             .map(CodeStackException::new)
                             .orElseGet(() -> new CodeStackException(new ErrorCode(SystemErrorCodeEnum.UNKNOW()))));
-            final String errMsg = Optional.of(se)
+            ResponseBodyBean<Object> result = new ResponseBodyBean<>();
+            result.setErrorCode(Optional.of(se)
+                    .map(CodeStackException::getValue)
+                    .map(Enumeration.Value::id)
+                    .orElseGet(SystemErrorCodeEnum.UNKNOW()::id));
+            result.setStackTrace(Optional.of(e)
+                    .map(error -> {
+                        final StringWriter sw = new StringWriter();
+                        e.printStackTrace(new PrintWriter(sw));
+                        return Stream.of(sw.toString().split("\n\t"))
+                                .skip(1)
+                                .map(str -> str.replace("\n",""))
+                                .toArray(String[]::new);
+                    }).orElse(null));
+            result.setMessage(Optional.of(se)
                     .map(CodeStackException::getValue)
                     .filter(value -> !value.equals(SystemErrorCodeEnum.UNKNOW()))
                     .map(Enumeration.Value::toString)
-                    .orElseGet(e::getMessage);
-            Optional.of(se)
-                    .map(CodeStackException::getValue)
-                    .map(Enumeration.Value::id)
-                    .ifPresent(result::setErrorCode);
-            StringWriter sw = new StringWriter();
-            PrintWriter pw = new PrintWriter(sw);
-            e.printStackTrace(pw);
-            result.setStackTrace(sw.toString().split("\n\t"));
-            result.setMessage(errMsg);
-            responseEntity = new ResponseEntity<>(result,HttpStatus.INTERNAL_SERVER_ERROR);
+                    .orElseGet(e::getMessage));
+            return new ResponseEntity<>(result, HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        return responseEntity;
     }
 
     /**
      * 找到第一个CodeException
      */
     private static CodeStackException findException(Throwable t) {
-        if (t == null) return null;
-        if (t instanceof CodeStackException) return (CodeStackException) t;
-        return findException(t.getCause());
+        return Optional.ofNullable(t)
+                .map(e -> e instanceof CodeStackException
+                        ? (CodeStackException) e
+                        : findException(e.getCause()))
+                .orElse(null);
     }
 }
