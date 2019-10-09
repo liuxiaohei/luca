d0fd9a9409c19c811757ba2831327822cc29f377
liuxiaohei <2297598383@qq.com>
2019-10-09 20:02:19 +0800

    adj

--- src/main/java/org/ld/actors/Counter.java
+++ src/main/java/org/ld/actors/Counter.java
@@ -14,7 +14,7 @@ public class Counter extends AbstractActor {
     public Receive createReceive() {
         return receiveBuilder()
                 .match(String.class, e -> getSender().tell("成功收到用户消息!", ActorRef.noSender()))
-                .matchAny(messages -> log.info("UserLoginActor 接收到消息:{}", messages))
+                .matchAny(messages -> System.out.println("received"))
                 .build();
     }
 
@@ -22,7 +22,6 @@ public class Counter extends AbstractActor {
     public static void main(String[] args) throws Exception {
         ActorSystem actorSystem = ActorSystem.create("actorSystemName");
         ActorRef userLoginActor = actorSystem.actorOf(Props.create(Counter.class, Counter::new), "userLoginActorName");
-        userLoginActor.tell("666", ActorRef.noSender());
         userLoginActor.tell(666, ActorRef.noSender());
         Thread.sleep(10);
         actorSystem.terminate(); // 这个方法终止 actor
