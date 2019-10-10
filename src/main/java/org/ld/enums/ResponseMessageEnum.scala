d7c6801d2ee34a2db46f29401b224515b8e9375b
liuxiaohei <2297598383@qq.com>
2019-10-10 10:23:01 +0800

    adj

--- src/main/java/org/ld/enums/ResponseMessageEnum.scala
+++ src/main/java/org/ld/enums/ResponseMessageEnum.scala
@@ -1,16 +1,18 @@
 package org.ld.enums
 
+import org.springframework.http.HttpStatus
+
 /*
  * @author ld
  */
 object ResponseMessageEnum extends Enumeration {
-  Value(200, "操作成功")
-  Value(400, "登录参数错误")
-  Value(401, "用户名或密码错误")
-  Value(403, "用户被禁止")
-  Value(404, "找不到资源")
-  Value(409, "业务逻辑异常")
-  Value(422, "参数校验异常")
-  Value(500, "服务器内部错误")
-  Value(503, "Hystrix异常")
+  Value(HttpStatus.OK.value(), "操作成功")
+  Value(HttpStatus.BAD_REQUEST.value(), "登录参数错误")
+  Value(HttpStatus.UNAUTHORIZED.value(), "用户名或密码错误")
+  Value(HttpStatus.FORBIDDEN.value(), "用户被禁止")
+  Value(HttpStatus.NOT_FOUND.value(), "找不到资源")
+  Value(HttpStatus.CONFLICT.value(), "业务逻辑异常")
+  Value(HttpStatus.UNPROCESSABLE_ENTITY.value(), "参数校验异常")
+  Value(HttpStatus.INTERNAL_SERVER_ERROR.value(), "服务器内部错误")
+  Value(HttpStatus.SERVICE_UNAVAILABLE.value(), "Hystrix异常")
 }
