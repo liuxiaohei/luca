9afdec3768b8ac2e53bc4a56d2e230afff295de1
liuxiaohei <2297598383@qq.com>
2019-10-23 13:50:20 +0800

    adj

--- src/main/java/org/ld/enums/ResponseMessageEnum.scala
+++ src/main/java/org/ld/enums/ResponseMessageEnum.scala
@@ -11,15 +11,21 @@ import scala.collection.JavaConverters._
  * @author ld
  */
 object ResponseMessageEnum extends Enumeration {
-  Value(HttpStatus.OK.value(), "操作成功")
-  Value(HttpStatus.BAD_REQUEST.value(), "登录参数错误")
-  Value(HttpStatus.UNAUTHORIZED.value(), "用户名或密码错误")
-  Value(HttpStatus.FORBIDDEN.value(), "用户被禁止")
-  Value(HttpStatus.NOT_FOUND.value(), "找不到资源")
-  Value(HttpStatus.CONFLICT.value(), "业务逻辑异常")
-  Value(HttpStatus.UNPROCESSABLE_ENTITY.value(), "参数校验异常")
-  Value(HttpStatus.INTERNAL_SERVER_ERROR.value(), "服务器内部错误")
-  Value(HttpStatus.SERVICE_UNAVAILABLE.value(), "Hystrix异常")
+
+  /**
+   * 隐式转换 HttpStatus => Int
+   */
+  implicit def getValue(a: HttpStatus): Int = a.value()
+
+  Value(HttpStatus.OK, "操作成功")
+  Value(HttpStatus.BAD_REQUEST, "登录参数错误")
+  Value(HttpStatus.UNAUTHORIZED, "用户名或密码错误")
+  Value(HttpStatus.FORBIDDEN, "用户被禁止")
+  Value(HttpStatus.NOT_FOUND, "找不到资源")
+  Value(HttpStatus.CONFLICT, "业务逻辑异常")
+  Value(HttpStatus.UNPROCESSABLE_ENTITY, "参数校验异常")
+  Value(HttpStatus.INTERNAL_SERVER_ERROR, "服务器内部错误")
+  Value(HttpStatus.SERVICE_UNAVAILABLE, "Hystrix异常")
 
   def getMessages: util.List[ResponseMessage] = values
     .map((e: Enumeration#Value) => new ResponseMessageBuilder().code(e.id).message(e.toString).responseModel(new ModelRef("ApiError")).build)
