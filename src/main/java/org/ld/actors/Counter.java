9bacf5dde99896b01589f53a768a847b63c464fe
liuxiaohei <2297598383@qq.com>
2019-10-26 00:35:13 +0800

    adj

--- src/main/java/org/ld/actors/Counter.java
+++ src/main/java/org/ld/actors/Counter.java
@@ -3,25 +3,40 @@ package org.ld.actors;
 import akka.actor.*;
 import org.ld.utils.Logger;
 
+import java.util.stream.IntStream;
 
+/**
+ * https://www.wanaright.com/2018/09/08/akka-java8/
+ */
 public class Counter extends AbstractActor {
 
     private static Logger log = Logger.newInstance();
 
+    private Integer i = 0;
+
     @Override
     public Receive createReceive() {
         return receiveBuilder()
-                .match(String.class, e -> getSender().tell("成功收到用户消息!", ActorRef.noSender()))
+                .match(User.class, e -> log.info(() -> "成功收到用户消息!" + e.getUserName() + "-" + i++))
+                .match(Other.class, e -> log.info(() -> "成功收到其它消息!" + e.getUserName() + "_" + i))
                 .matchAny(messages -> log.info(messages::toString))
                 .build();
     }
 
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws InterruptedException {
         ActorSystem actorSystem = ActorSystem.create("actorSystemName");
         ActorRef userLoginActor = actorSystem.actorOf(Props.create(Counter.class, Counter::new), "userLoginActorName");
-        userLoginActor.tell(666, ActorRef.noSender());
-        Thread.sleep(10);
+        IntStream.rangeClosed(0, 100).parallel().forEach(e -> {
+                    userLoginActor.tell(new User(1, "Dong" + e, 30), ActorRef.noSender());
+                    userLoginActor.tell(new User(1, "Dong" + e, 30), ActorRef.noSender());
+                    try {
+                        Thread.sleep(10);
+                    } catch (Exception ignored) {
+                    }
+                }
+        );
+        Thread.sleep(1000);
         actorSystem.terminate(); // 这个方法终止 actor
     }
 }
